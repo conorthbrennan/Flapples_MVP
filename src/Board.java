c2122be731f17fea672860d45993694ce0899a35
Rebecca Thomas <rathomas99@gmail.com>
2014-03-10 14:10:32 -0400

    Board has addCard now.

--- src/Board.java
+++ src/Board.java
@@ -9,4 +9,27 @@ public class Board {
 	private ArrayList<Player> players;
 	private Deck goals, rules, discard, deckdeck;
 	
+	public Deck getDeckDeck(){
+		return deckdeck;
+	}
+	
+	public ArrayList<Player> getPlayers(){
+		return players;
+	}
+	
+	public Deck getGoals(){
+		return goals;
+	}
+	
+	public Deck getRules(){
+		return rules;
+	}
+	
+	public Deck getDiscards(){
+		return discard;
+	}
+	
+	public void addCard(Card c, Deck receiver){
+		receiver.addCard(c);
+	}
 }
