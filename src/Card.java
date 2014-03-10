55b46be9c55b5b1f4e5706783a9c3a45e10629b7
conorthbrennan <conor492@gmail.com>
2014-03-10 10:34:09 -0400

    added getters and setters

--- src/Card.java
+++ src/Card.java
@@ -6,4 +6,37 @@ public abstract class Card {
 	BufferedImage picture;
 	String description;
 	int place, ID;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public BufferedImage getPicture() {
+		return picture;
+	}
+	public void setPicture(BufferedImage picture) {
+		this.picture = picture;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getPlace() {
+		return place;
+	}
+	public void setPlace(int place) {
+		this.place = place;
+	}
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	
+	
 }
