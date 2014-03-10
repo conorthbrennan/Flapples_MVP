a48a2333f8d33ee8ac768d34d7d3fc3e56da492d
conorthbrennan <conor492@gmail.com>
2014-03-10 13:27:47 -0400

    added draw card added get deck

--- src/Deck.java
+++ src/Deck.java
@@ -1,8 +1,16 @@
 import java.util.ArrayList;
 
-
-public abstract class Deck {
+/**
+ * 
+ * @author Conor Brennan, Becky Thomas, Billy Leete
+ *
+ */
+public class Deck {
 	private ArrayList<Card> deck = new ArrayList<Card>();
+	
+	public ArrayList<Card> getDeck(){
+		return deck;
+	}
 	
 	/**
 	 * shuffles the deck
@@ -44,8 +52,13 @@ public abstract class Deck {
 	/**
 	 * Takes a card from a deck and places it in another
 	 */
-	public void drawCard(Deck reciever){
-		
-		
-	}
+	public void drawCard(Deck receiver, int num){
+		ArrayList<Card> receiverDeck = receiver.getDeck();//gets the deck list from the receiving deck
+		for (int i = 0; i < num; i++) {
+			Card drawn = deck.remove(0);
+			receiverDeck.add(drawn);
+		}//end for
+	}//class drawCard
+	
+	
 }
