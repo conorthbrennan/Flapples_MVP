54f86f63bd3100048f66d379b0b7029808c51717
conorthbrennan <conor492@gmail.com>
2014-03-18 10:32:10 -0400

    apparently someone was working with an old version of deck so it was missing a lot of things. I fixed it

--- src/Deck.java
+++ src/Deck.java
@@ -67,22 +67,77 @@ public class Deck implements GameObject {
 	
 	/**
 	 * Takes a card from a deck and places it in another
+	 * @param reciever - the deck receiving the card
+	 * @param num - the number of cards to be drawn
 	 */
-	public void drawCard(Deck receiver, int num)	// [wgl] Hey! we don't need iteration here, do we?
-	{		// [wgl] can you withdraw a *specific* card? Like, by its 'place' or 'ID'?
-		ArrayList<Card> receiverDeck = receiver.getDeck();//gets the deck list from the receiving deck
+	public void drawCard(Deck receiver, int num){
 		for (int i = 0; i < num; i++) {
 			Card drawn = deck.remove(0);//takes a card from the deck
-			receiverDeck.add(drawn);//puts card in receiving deck
+			receiver.addCard(drawn);//puts card in receiving deck
 		}//end for
 	}//class drawCard
 	
 	/**
-	 * adds card to a deck
+	 * adds card to the deck
 	 * @param card - card to be added
 	 */
 	public void addCard(Card card){
-		deck.add(card);
+		deck.add(card);//adds the card to the deck
+	}//class addCard
+	
+	/**
+	 * adds an array of cards to the deck
+	 * @param cards - cards to be added
+	 */
+	public void addCards(Card[] cards){
+		for (int i = 0; i < cards.length; i++) {//iterates through the list of cards
+			addCard(cards[i]);//adds each card to the deck
+		}//end for
+	}//class addCards
+	
+	/**
+	 * removes a card from the deck
+	 * @param card - card to be removed
+	 */
+	public void removeCard(Card card){
+		if(deck.indexOf(card) != -1)//ensures the card is in the deck
+			deck.remove(card);//removes the card from the deck
+	}//class removeCard
+	
+	/**
+	 * removes an array of cards to the deck
+	 * @param cards - cards to be removed
+	 */
+	public void removeCards(Card[] cards){
+		for (int i = 0; i < cards.length; i++) {//iterates through the list of cards
+			addCard(cards[i]);//removes each card from the deck
+		}//end for
+	}//class addCards
+	
+	/**
+	 * searches the deck for a specific card
+	 * @param ID - the ID of the desired card
+	 * @return card - the card with the desired ID if it is in the deck
+	 * 				  if it's not in the deck the function returns null
+	 */
+	public Card search(int ID){
+		Card card = null;// Initializes the card to be returned as null
+		for (int i = 0; i < deck.size(); i++) {//iterates through the whole deck until the card is found
+			Card drawn = deck.get(i);//gets the card from the deck
+			if(drawn.getID() == ID){//checks the ID of the drawn card against the ID of the desired card
+				card = drawn;//sets the drawn card as the card to be returned
+				break;//exits the for loop
+			}//end if
+		}//end for
+		return card;//returns the card
+	}//class search
+	
+	/**
+	 * counts the number of cards in the deck
+	 * @return the number of cards in the deck
+	 */
+	public int count(){
+		return deck.size();//returns the number of cards in the deck
 	}
 	
 }
