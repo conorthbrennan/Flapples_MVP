25170f0e9c663cdae3e5d9cee9e91d9ea2334143
rathomas99 <rathomas99@gmail.com>
2014-03-28 19:51:38 -0400

    I made it so that the graphics is more like what we actually want. I haven't filled in all the methods yet.

--- src/OverallRunner.java
+++ src/OverallRunner.java
@@ -9,6 +9,7 @@ import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.UIManager;
 
 public class OverallRunner
@@ -16,16 +17,36 @@ public class OverallRunner
 
 	public static void main(String [] args)
 	{
+		Game g = new Game();
+		//StandardDeck sd = new StandardDeck();
+		Deck sd = new Deck(g);
+		sd = setUpStandardDeck(sd);//CONOR WRITE THIS CODE FOR ME!
 		
+		//all sorts of stuff here
+		//each player should get 3 cards to start off with
 		
 		
 		//I shall use JPanes and JFrames to display everything. 
+		//This will need to take in which Player is going in order to work properly.
 		drawEverything();
 
 
 
 	}
+	
+	/**
+	 * This makes the incoming deck the original set of cards.
+	 * @param sd
+	 * @return the initial deck of cards.
+	 */
+	private static Deck setUpStandardDeck(Deck sd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
+	/**
+	 * This draws everything.
+	 */
 	private static void drawEverything() {
 		//set the look and feel
 		try{
@@ -37,13 +58,33 @@ public class OverallRunner
 		}
 
 		JFrame testframe = new JFrame("Flapples");
-		Container pane = testframe.getContentPane();
+		Container uberpane = testframe.getContentPane();
 
 		//FlowLayout flow = new FlowLayout(FlowLayout.RIGHT,200,20);//alignment,hgap,vgap		
-		BoxLayout box = new BoxLayout(pane,BoxLayout.LINE_AXIS);
-		pane.setLayout(box);
+		BoxLayout box = new BoxLayout(uberpane,BoxLayout.PAGE_AXIS);//top to bottom
+		uberpane.setLayout(box);
 
-		JButton top = new JButton("Top");
+		JPanel playerInfoRow = setUpPlayerInfoRow();//This will hold the player's name at the top of the screen.
+		JPanel goalsRow = setUpGoalsRow();//This will hold the goals in the second row.
+		JPanel rulesRow = setUpRulesRow();//This will hold the rules in the third row.
+		JPanel discardRow = setUpDiscardPileRow();//This will hold the discard pile in the fourth row.
+		JPanel holdingPenRow = setUpHoldingPenRow();//This will be the player's holding pen in the fifth row.
+		JPanel handRow = setUpHandRow();//This will hold the player's hand.
+		
+		//Add all the items to the pane
+		uberpane.add(playerInfoRow);
+		uberpane.add(goalsRow);
+		uberpane.add(rulesRow);
+		uberpane.add(discardRow);
+		uberpane.add(holdingPenRow);
+		uberpane.add(handRow);
+		
+		testframe.pack();
+		testframe.setVisible(true);
+		testframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+/*		JButton top = new JButton("Top");
 		JButton bot = new JButton("Bottom");
 
 		ActionListener alist = new ActionListener(){
@@ -80,13 +121,32 @@ public class OverallRunner
 
 		top.addMouseListener(mlist1);
 		bot.addMouseListener(mlist2);
-
-		//Add all the items to the pane
-		pane.add(top);
-		pane.add(bot);
-
-		testframe.pack();
-		testframe.setVisible(true);
-		testframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+*/
+		
 	}
+	private static JPanel setUpHandRow() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	private static JPanel setUpHoldingPenRow() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	private static JPanel setUpDiscardPileRow() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	private static JPanel setUpRulesRow() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	private static JPanel setUpGoalsRow() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	private static JPanel setUpPlayerInfoRow() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
