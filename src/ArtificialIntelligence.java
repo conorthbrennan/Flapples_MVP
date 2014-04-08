9964fe7f7369cef71a270b55e130d7e629cdb429
conorthbrennan <conor492@gmail.com>
2014-04-08 13:16:12 -0400

    artificial intelligence updated

--- src/ArtificialIntelligence.java
+++ src/ArtificialIntelligence.java
@@ -1,10 +1,12 @@
 
 public class ArtificialIntelligence extends Player{
 	int difficulty;//accepts an Integer from 0 - 4 with 0 being super easy and 4 being mega hard
+	Game game;
 
-	public ArtificialIntelligence(String n, int diff){
+	public ArtificialIntelligence(String n, int diff, Game g){
 		super(n);
 		difficulty = diff;
+		game = g;
 	}
 
 	public static void main(String[] args) {
@@ -13,17 +15,49 @@ public class ArtificialIntelligence extends Player{
 	}
 	
 	public Card PickCardSwitch(){
-		Card picked = null;
+		Card picked = null;//the card that is picked
 		
+		//chooses which pick card method to use based on AI difficulty
 		switch(difficulty){
 		
-		case 0: 
+		case 0: picked = PickCardVeryEasy();
+				break;
+				
+		case 1: picked = PickCardEasy();
+				break;
+				
+		case 2: picked = PickCardNormal();
+				break;
+				
+		case 3: picked = PickCardHard();
+				break;
+				
+		case 4: picked = PickCardVeryHard();
+				break;
 		}
 		
 		return picked;
 	}
 	
 	public Card PickCardVeryEasy(){
+		int index = (int) (Math.random() * hand.count());
+		Card picked = hand.getDeck().get(index);
+		return picked;
+	}
+	
+	public Card PickCardEasy(){
+		return null;
+	}
+	
+	public Card PickCardNormal(){
+		return null;
+	}
+	
+	public Card PickCardHard(){
+		return null;
+	}
+	
+	public Card PickCardVeryHard(){
 		return null;
 	}
 
